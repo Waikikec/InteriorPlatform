f9fbb8e549fb6e12a23cab9c526e46f34831496a
Martin Yankov <martin.d.yankov@gmail.com>
2022-03-29 11:56:19 +0300

    Fixed Automapper to get Styles from Database

--- Web/InteriorPlatform.Web.ViewModels/Project/EditProjectInputModel.cs
+++ Web/InteriorPlatform.Web.ViewModels/Project/EditProjectInputModel.cs
@@ -2,12 +2,13 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-
+    using System.Linq;
+    using AutoMapper;
     using InteriorPlatform.Data.Models;
     using InteriorPlatform.Services.Mapping;
     using Microsoft.AspNetCore.Http;
 
-    public class EditProjectInputModel : IMapFrom<Project>
+    public class EditProjectInputModel : IMapFrom<Project>, IHaveCustomMappings
     {
         [Required(ErrorMessage = "Полето '{0}' е задължително.")]
         [Display(Name = "Име")]
@@ -33,10 +34,16 @@
 
         public IEnumerable<KeyValuePair<string, string>> CategoriesItems { get; set; }
 
-        public IEnumerable<string> Styles { get; set; }
+        public IEnumerable<int> StylesName { get; set; }
 
         public IEnumerable<KeyValuePair<string, string>> StylesItems { get; set; }
 
-        public IEnumerable<IFormFile> Images { get; set; }
+        public IEnumerable<Image> Images { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Project, EditProjectInputModel>()
+                .ForMember(x => x.StylesName, opt => opt.MapFrom(x => x.Styles.Select(t => t.Id).ToList()));
+        }
     }
 }
