dcfef72e7f47134c26d0b85140ed111ee538d534
Waikikec <martin.d.yankov@gmail.com>
2022-04-04 13:39:43 +0300

    Added Contact Page and Re-arrange Home Index Page

--- InteriorPlatform.Common/GlobalConstants.cs
+++ InteriorPlatform.Common/GlobalConstants.cs
@@ -5,5 +5,7 @@
         public const string SystemName = "FAB Дизайн";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string GoogleMapsApiKey = "AIzaSyCm-RDiTEcNyDtr3KM7R-fxxD0QEwEjiyI";
     }
 }
