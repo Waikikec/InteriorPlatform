2ad18734d1fe921e50a7484bfea593ad45af9db0
Waikikec <martin.d.yankov@gmail.com>
2022-04-10 21:51:46 +0300

    Categories Service Test

---
+++ Tests/InteriorPlatform.Services.Data.Tests/CategoriesServiceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace InteriorPlatform.Services.Data.Tests
+{
+    using System.Linq;
+
+    using InteriorPlatform.Data;
+    using InteriorPlatform.Data.Models;
+    using InteriorPlatform.Data.Repositories;
+    using Microsoft.EntityFrameworkCore;
+    using Xunit;
+
+    public class CategoriesServiceTests
+    {
+        [Fact] // IEnumerable<KeyValuePair<string, string>> GetCategories()
+        public async void GetCategoriesCount()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "InteriorPlatformDB").Options;
+
+            using var dbContext = new ApplicationDbContext(options);
+            dbContext.Categories.Add(new Category());
+            dbContext.Categories.Add(new Category());
+            dbContext.Categories.Add(new Category());
+            await dbContext.SaveChangesAsync();
+
+            using var repository = new EfDeletableEntityRepository<Category>(dbContext);
+            var service = new CategoriesService(repository);
+            Assert.Equal(3, service.GetCategories().Count());
+        }
+    }
+}
