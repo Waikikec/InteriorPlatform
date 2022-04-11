f316796fecf4627afc1d823dceccb9f03858be6f
Martin Yankov <martin.d.yankov@gmail.com>
2022-04-11 18:46:17 +0300

    First Steps with Testing

---
+++ Tests/InteriorPlatform.Services.Data.Tests/CategoriesServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using InteriorPlatform.Data.Common.Repositories;
+using InteriorPlatform.Data.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace InteriorPlatform.Services.Data.Tests
+{
+    public class CategoriesServiceTests
+    {
+        [Fact]
+        public void GetCategoriesCountCheck()
+        {
+
+        }
+    }
+}
---
+++ Tests/InteriorPlatform.Services.Data.Tests/ProjectsServiceTests.cs
@@ -0,0 +1,59 @@
+﻿namespace InteriorPlatform.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using InteriorPlatform.Data;
+    using InteriorPlatform.Data.Models;
+    using InteriorPlatform.Data.Repositories;
+    using InteriorPlatform.Web.ViewModels.Project;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Http.Internal;
+    using Microsoft.EntityFrameworkCore;
+    using Moq;
+    using Xunit;
+
+    public class ProjectsServiceTests
+    {
+        [Fact] // public async Task CreateAsync(CreateProjectInputModel model, ApplicationUser user, string imagePath)
+        public async Task CreateAsyncShouldAddProjectSuccessfully()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            var dbContext = new ApplicationDbContext(options);
+
+            var projectsRepo = new EfDeletableEntityRepository<Project>(dbContext);
+            var stylesRepo = new EfDeletableEntityRepository<Style>(dbContext);
+
+            var service = new ProjectsService(projectsRepo, stylesRepo);
+            var user = new ApplicationUser();
+
+            var project = new CreateProjectInputModel
+            {
+                Name = "LAKE HOUSE INTERIORS",
+                Description = "КОПРОРАТИВЕН ОФИС FADATA СОФИЯ Офис пространство съобразено с нуждите и изискванията на AGILE WORKING SPACE.Оазис за сетивата и креативността.",
+                IsRealized = "true",
+                IsPublic = "true",
+                CategoryId = 1,
+            };
+
+            project.Styles = new List<string> { "1", "2", };
+
+            var bytes = Encoding.UTF8.GetBytes("This is a dummy file");
+            IFormFile file = new FormFile(new MemoryStream(bytes), 0, bytes.Length, "Data", "dummy.jpg")
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = "multipart/form-data",
+            };
+
+            project.Images = new List<IFormFile> { file };
+
+            await service.CreateAsync(project, user, "wwwroot");
+
+            Assert.Equal(1, service.GetCount());
+        }
+    }
+}
