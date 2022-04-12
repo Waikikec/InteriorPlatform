011e6069fe7475514969209ce1ca9e2fcdf1f1ee
Waikikec <martin.d.yankov@gmail.com>
2022-04-12 23:31:06 +0300

    Remove unnecessary code

--- Tests/InteriorPlatform.Services.Data.Tests/CategoriesServiceTests.cs
+++ Tests/InteriorPlatform.Services.Data.Tests/CategoriesServiceTests.cs
@@ -2,14 +2,11 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     using InteriorPlatform.Data;
-    using InteriorPlatform.Data.Common.Repositories;
     using InteriorPlatform.Data.Models;
     using InteriorPlatform.Data.Repositories;
     using Microsoft.EntityFrameworkCore;
-    using Moq;
     using Xunit;
 
     public class CategoriesServiceTests
@@ -22,17 +19,6 @@
             var dbContext = new ApplicationDbContext(options);
 
             var categoriesRepository = new EfDeletableEntityRepository<Category>(dbContext);
-
-            var list = new List<Category>();
-            var mockRepo = new Mock<IRepository<Category>>();
-
-            //mockRepo
-            //    .Setup(x => x.All())
-            //    .Returns(list.AsQueryable());
-
-            //mockRepo
-            //    .Setup(x => x.AddAsync(It.IsAny<Category>()))
-            //    .Callback((Category category) => list.Add(category));
 
             var service = new CategoriesService(categoriesRepository);
 
