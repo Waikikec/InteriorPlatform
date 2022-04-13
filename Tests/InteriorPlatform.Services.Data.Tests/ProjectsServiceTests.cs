9d1b03ebe15293fe39fda8114393d5505b1c5147
Martin Yankov <martin.d.yankov@gmail.com>
2022-04-13 16:31:30 +0300

    Create Project Async Test

--- Tests/InteriorPlatform.Services.Data.Tests/ProjectsServiceTests.cs
+++ Tests/InteriorPlatform.Services.Data.Tests/ProjectsServiceTests.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
@@ -16,20 +17,32 @@
     using Moq;
     using Xunit;
 
-    public class ProjectsServiceTests
+    public class ProjectsServiceTests : IDisposable
     {
-        [Fact] // public async Task CreateAsync(CreateProjectInputModel model, ApplicationUser user, string imagePath)
-        public async Task AddingProjectSuccessfully()
+        private readonly ApplicationDbContext dbContext;
+        private readonly EfDeletableEntityRepository<Project> projectsRepository;
+        private readonly EfDeletableEntityRepository<Style> stylesRepository;
+
+        public ProjectsServiceTests()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
-            var dbContext = new ApplicationDbContext(options);
 
-            var projectsRepo = new EfDeletableEntityRepository<Project>(dbContext);
-            var stylesRepo = new EfDeletableEntityRepository<Style>(dbContext);
+            this.dbContext = new ApplicationDbContext(options);
 
-            var service = new ProjectsService(projectsRepo, stylesRepo);
+            this.projectsRepository = new EfDeletableEntityRepository<Project>(this.dbContext);
+            this.stylesRepository = new EfDeletableEntityRepository<Style>(this.dbContext);
+
+            //AutoMapperConfig.RegisterMappings(typeof(SingleDesignerViewModel).Assembly, typeof(ApplicationUser).Assembly);
+        }
+
+        [Fact] // public async Task CreateAsync(CreateProjectInputModel model, ApplicationUser user, string imagePath)
+        public async Task AddingProjectSuccessfully()
+        {
             var user = new ApplicationUser();
+            user.TownId = 1;
+            user.CompanyId = 2;
+            user.Id = Guid.NewGuid().ToString();
 
             var project = new CreateProjectInputModel
             {
@@ -39,21 +52,40 @@
                 IsPublic = "true",
                 CategoryId = 1,
             };
+            project.Styles = new List<string> { "1" };
 
-            project.Styles = new List<string> { "1", "2", };
+            var style1 = new Style()
+            {
+                Id = 1,
+                Name = "Бохо",
+            };
+
+            await this.stylesRepository.AddAsync(style1);
+            await this.stylesRepository.SaveChangesAsync();
 
             var bytes = Encoding.UTF8.GetBytes("This is a dummy file");
             IFormFile file = new FormFile(new MemoryStream(bytes), 0, bytes.Length, "Data", "dummy.jpg")
             {
                 Headers = new HeaderDictionary(),
-                ContentType = "multipart/form-data",
+                ContentType = "image/jpg",
+                ContentDisposition = @"form-data; name='Data'; filename='dummy.jpg'",
             };
 
             project.Images = new List<IFormFile> { file };
 
+            var service = new ProjectsService(this.projectsRepository, this.stylesRepository);
             await service.CreateAsync(project, user, "wwwroot");
 
-            Assert.Equal(1, service.GetCount());
+            Assert.Equal(1, this.projectsRepository.All().Count());
+        }
+
+        public void Dispose()
+        {
+            this.dbContext.Database.EnsureDeleted();
+            this.dbContext.Dispose();
+
+            this.projectsRepository.Dispose();
+            this.stylesRepository.Dispose();
         }
     }
 }
